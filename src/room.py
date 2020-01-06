0733beb413acaaa13d795526726f7bd1a092738f
Desiree Morris <desiquinncompany@gmail.com>
2020-01-05 20:57:02 -0500

    created player and room classes

--- src/room.py
+++ src/room.py
@@ -1,2 +1,19 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        # self.n_to = n_to
+        # self.s_to = s_to
+        # self.e_to = e_to
+        # self.w_to = w_to
+    
+    # def n_to(self, room):
+    
+    # def s_to(self, room):
+    
+    # def e_to(self, room):
+
+    # def w_to(self, room):
